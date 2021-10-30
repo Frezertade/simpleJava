08fb389d25a4254d64ee4e73ab4543c852d9761d
Frezer Kifle <frezertade@gmail.com>
2021-10-30 13:39:44 -0400

    simple Java Hello world application with simple test

---
+++ src/test/java/hello/GreeterTest.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GreeterTest {
+    private Greeter greeter = new Greeter();
+    @Test
+    public void greeterSayHello(){
+        assertThat(greeter.sayHello(),containsString("Hello"));
+    }
+}
